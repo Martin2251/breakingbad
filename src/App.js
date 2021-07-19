73d7a00a95b711d6acd63ea3f94cf59073e5ce15
martin lowe <martinlowe673@gmail.com>
2021-07-19 08:38:38 +0100

    added router but set uo needs to be better

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React, { useEffect, useState } from "react";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+  useParams
+} from "react-router-dom";
+
+function CharacterPage(){
+  let { id } = useParams();
+  const [characterData, setCharacterData] = useState("");
+
+  useEffect(function () {
+    fetch("https://www.breakingbadapi.com/api/characters/" + id)
+      .then(function (response) {
+        return response.json();
+      })
+      .then((data) => {
+        setListCharacters(data);
+      });
+  }, []);
+  return <div>
+  <p>{characterData.name} </p>
+  </div>
+}
 
 function Card(props) {
-  return (
+  return
+    <Link to={`/character/${props.id}`}>
     <div>
       <h4>Name: {props.character.name}</h4>
       <p>Birthday: {props.character.birthday}</p>
@@ -11,8 +38,8 @@ function Card(props) {
       <p>Nickname: {props.character.nickname}</p>
       <p>Occupation: {props.character.occupation}</p>
     </div>
-  );
-}
+  </Link>
+};
 
 function App() {
   const [listCharacters, setListCharacters] = useState([]);
@@ -27,12 +54,17 @@ function App() {
       });
   }, []);
 
+
+
   return (
+
     <div className="App">
+    <Router>
       {listCharacters.map(function (character) {
         return <Card character={character}></Card>;
       })}
     </div>
+    </Router>
   );
 }
 
