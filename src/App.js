8e73f8d1d2896cf7ee879e4aa0697b9a1eb2698a
martin lowe <martinlowe673@gmail.com>
2021-07-20 08:52:23 +0100

    working but not showing card page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React, { useEffect, useState } from "react";
 import {
@@ -9,36 +8,42 @@ import {
   useParams,
 } from "react-router-dom";
 
-function CharacterPage(){
+function CharacterPage() {
   let { id } = useParams();
   const [characterData, setCharacterData] = useState("");
 
-  useEffect(function () {
-    fetch("https://www.breakingbadapi.com/api/characters/" + id)
-      .then(function (response) {
-        return response.json();
-      })
-      .then((data) => {
-        setCharacterData(data);
-      });
-  }, []);
-  return <div>
-  <p>{characterData.name} </p>
-  </div>
+  useEffect(
+    function () {
+      fetch("https://www.breakingbadapi.com/api/characters/" + id)
+        .then(function (response) {
+          return response.json();
+        })
+        .then((data) => {
+          setCharacterData(data);
+        });
+    },
+    [id]
+  );
+  return (
+    <div>
+      <p>{characterData.name} </p>
+    </div>
+  );
 }
 
 function Card(props) {
-  return
+  return (
     <Link to={`/character/${props.id}`}>
-    <div className="card">
-      <h4>Name: {props.character.name}</h4>
-      <p>Birthday: {props.character.birthday}</p>
-      <p>Status: {props.character.status}</p>
-      <p>Nickname: {props.character.nickname}</p>
-      <p>Occupation: {props.character.occupation}</p>
-    </div>
-  </Link>
-};
+      <div className="card">
+        <h4>Name: {props.character.name}</h4>
+        <p>Birthday: {props.character.birthday}</p>
+        <p>Status: {props.character.status}</p>
+        <p>Nickname: {props.character.nickname}</p>
+        <p>Occupation: {props.character.occupation}</p>
+      </div>
+    </Link>
+  );
+}
 
 function App() {
   const [listCharacters, setListCharacters] = useState([]);
@@ -53,23 +58,21 @@ function App() {
       });
   }, []);
 
-
-
   return (
-
     <Router>
-    <Route path="/character/:id">
-    <CharacterPage></CharacterPage>
-    </Route>
-    <Route to="/">
-    <div className="App">
-      {listCharacters.map(function (character) {
-        return <Card character={character}></Card>;
-      })}
-    </div>
-
-
-    </Route>
+      <Switch>
+        <Route path="/character/:id">
+          <CharacterPage></CharacterPage>
+        </Route>
+        <Route to="/">
+          <div className="App">
+            <h1>Breaking Bad API</h1>
+            {listCharacters.map(function (character) {
+              return <Card character={character}></Card>;
+            })}
+          </div>
+        </Route>
+      </Switch>
     </Router>
   );
 }
